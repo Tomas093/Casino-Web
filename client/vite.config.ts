34510a8e2840eef1e915034ba571dc7f977070e8
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-04-08 11:27:26 -0300

    Enhance footer layout and content; update styles for improved responsiveness

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite'
     import path from 'path'
 
     export default defineConfig({
-      base: "/Casino-Web",
-        plugins: [react()],
+      plugins: [react()],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, 'src')
