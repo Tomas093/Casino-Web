47c542ef4655209f42f7337b9ee6e12b22cf5ee2
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-05-06 11:32:17 -0300

    Update footer links; change support link to English and update location link to Google Maps

--- client/src/React/components/Footer.tsx
+++ client/src/React/components/Footer.tsx
@@ -28,9 +28,9 @@ const Footer: React.FC = () => {
                 <div className="footer-section footer-Contact">
                     <h3>Contacto</h3>
                     <ul>
-                        <li><a href="/soporte"><FaHeadset className="icon" />Soporte</a></li>
+                        <li><a href="/support"><FaHeadset className="icon" />Soporte</a></li>
                         <li><a href="tel:+123456789"><FaPhone className="icon" /> +54 91130893524</a></li>
-                        <li><a href="/ubicacion"><FaMapMarkerAlt className="icon" /> Buenos Aires, Argentina</a></li>
+                        <li><a href="https://www.google.com/maps/place/X5113+Salsipuedes,+C%C3%B3rdoba/@-31.1368955,-64.3067547,15z/data=!3m1!4b1!4m6!3m5!1s0x943281c901c86055:0xd2bd0375d8425052!8m2!3d-31.1368964!4d-64.2964335!16s%2Fg%2F11bx5q1ddj?hl=es&entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer"><FaMapMarkerAlt className="icon" /> Argentina, Córdoba</a></li>
                     </ul>
                 </div>
 
